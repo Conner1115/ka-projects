71315ad5b2e774bfc6c5b0a835c7d13eb9524095
Conner Ow <connerow1115@gmail.com>
2020-05-12 11:55:44 -0500

    Create security.js

---
+++ security.js
@@ -0,0 +1,3 @@
+if(document.getElementById("corner-logo").src !== "https://www.khanacademy.org/computer-programming/my-logo/6657230242725888/latest.png"){
+                alert("Do not alter the image source.  Reload the page.");
+            }
