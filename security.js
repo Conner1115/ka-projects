79b49a490570018b7d9533bd76ac8bdace5cd284
Conner Ow <connerow1115@gmail.com>
2020-05-12 11:58:13 -0500

    Update security.js

--- security.js
+++ security.js
@@ -1,3 +1,12 @@
 if(document.getElementById("corner-logo").src !== "https://www.khanacademy.org/computer-programming/my-logo/6657230242725888/latest.png"){
                 alert("Do not alter the image source.  Reload the page.");
+  alert("Do not alter the image source.  Reload the page.");
+  alert("Do not alter the image source.  Reload the page.");
+  alert("Do not alter the image source.  Reload the page.");
+  alert("Do not alter the image source.  Reload the page.");
+  alert("Do not alter the image source.  Reload the page.");
+  alert("Do not alter the image source.  Reload the page.");
+  alert("Do not alter the image source.  Reload the page.");
+  alert("Do not alter the image source.  Reload the page.");
+  alert("Do not alter the image source.  Reload the page.");
             }
